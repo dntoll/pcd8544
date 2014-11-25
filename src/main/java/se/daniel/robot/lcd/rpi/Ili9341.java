db700653e0f49b22178e27cc7c06ded3d88f9902
dntoll <daniel@familjentoll.se>
2014-11-25 21:45:24 +0100

    another test

--- src/main/java/se/daniel/robot/lcd/rpi/Ili9341.java
+++ src/main/java/se/daniel/robot/lcd/rpi/Ili9341.java
@@ -300,8 +300,8 @@ public class Ili9341 extends AbstractLcd {
 	  {
 	    for (j=0;j<240;j++)
 	    {
-	    	if (j < buffer.getWidth() && i < buffer.getHeight()) {
-		    	if (buffer.getPixel(j, i) ) {
+	    	if (i < buffer.getWidth() && j < buffer.getHeight()) {
+		    	if (buffer.getPixel(i, j) ) {
 		    		LCD_Write_DATA(0xFF);
 		    		LCD_Write_DATA(0xFF);
 		    	} else {
