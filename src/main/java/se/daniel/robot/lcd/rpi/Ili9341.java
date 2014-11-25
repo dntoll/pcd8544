579cba08769d319e5fff75b0db2492bc05790909
dntoll <daniel@familjentoll.se>
2014-11-25 22:42:51 +0100

    16000 px buffer

--- src/main/java/se/daniel/robot/lcd/rpi/Ili9341.java
+++ src/main/java/se/daniel/robot/lcd/rpi/Ili9341.java
@@ -23,7 +23,7 @@ public class Ili9341 extends AbstractLcd {
 	
 	int spiChannel;
 	byte[] buff;
-	int buffersize = 8000 ;
+	int buffersize = 4000 ;
 	/**
 	 * Default Constructor starting lcd on Spi channel 0 and with a speed of 4000000
 	 * @throws Exception 
