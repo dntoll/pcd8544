bbd9897ae4675f841d3abed3049e913f90b5ee55
dntoll <daniel@familjentoll.se>
2014-11-25 21:23:58 +0100

    another test

--- src/main/java/se/daniel/robot/App.java
+++ src/main/java/se/daniel/robot/App.java
@@ -19,16 +19,21 @@ public class App
     {
         //Multi platform LCD
     	//On PC its shown as a Window
-    	
+    	System.out.println("Starting up");
     	AbstractLcd lcd = LCDFactory.createLcd();
     	
     	
-    	
+    	System.out.println("cls");
 		lcd.cls();
+		
+		System.out.println("brightness");
 		lcd.set_brightness(512);
 		BufferedImage img = null;
 		try {
+			System.out.println("read image");
 		    img = ImageIO.read(new File(args[0]));
+		    
+		    System.out.println("show image");
 		    lcd.show_image(img);
 		} catch (IOException e) {
 			
@@ -37,6 +42,8 @@ public class App
 		lcd.text("012");
 		lcd.gotorc(2,2);
 		lcd.text("Hello World");*/
+		
+		System.out.println("brightness");
 		lcd.set_brightness(512);
 		
 		/**/
